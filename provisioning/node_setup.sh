0446230e28eba86df787094fe7865fb981ac5ab6
Janika <janika.liiv@gmail.com>
2015-02-24 15:34:59 +0100

    indent using spaces

--- provisioning/node_setup.sh
+++ provisioning/node_setup.sh
@@ -5,10 +5,10 @@ nodeVersion=v0.12.0
 nodeVersionFolder=node-$nodeVersion
 
 if [ ! -h /usr/bin/node ] && [ ! -f /usr/bin/node ]; then
-	echo "Installing node $nodeVersion from source"
-	cd
-	wget -nv http://nodejs.org/dist/$nodeVersion/$nodeVersionFolder.tar.gz
-	tar -zxf $nodeVersionFolder.tar.gz
-	cd $nodeVersionFolder
-	./configure && make && sudo make install
+  echo "Installing node $nodeVersion from source"
+  cd
+  wget -nv http://nodejs.org/dist/$nodeVersion/$nodeVersionFolder.tar.gz
+  tar -zxf $nodeVersionFolder.tar.gz
+  cd $nodeVersionFolder
+  ./configure && make && sudo make install
 fi
